9c3bad838838148c0b5ef8574d2e76851d61cb36
Nikhil Katre <nkatre@ncsu.edu>
2015-02-13 22:26:42 -0500

    completed homework 2

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var esprima = require("esprima");
-var options = {tokens:true, tolerant: true, loc: true, range: true };
+var options = {tokens : true, tolerant: true, loc: true, range: true };
 var faker = require("faker");
 var fs = require("fs");
 faker.locale = "en";
@@ -8,8 +8,11 @@ var _ = require('underscore');
 
 function main()
 {
-	var args = process.argv.slice(2);
-
+	var args = process.argv.slice(2); // selects all the elements from index position 2
+/*
+var arguments = process.argv.slice(2);
+Note that the first arg is usually the path to nodejs, and the second arg is the location of the script you're executing.
+*/
 	if( args.length == 0 )
 	{
 		args = ["subject.js"];
@@ -18,8 +21,7 @@ function main()
 
 	constraints(filePath);
 
-	generateTestCases()
-
+	generateTestCases();
 }
 
 
@@ -46,7 +48,14 @@ var mockFileLibrary =
 		{	
   			file1: 'text content',
 		}
-	}
+	},
+    emptyFile:                  // attribute where the file is without any content
+    {
+         pathContent: 
+		{	
+  			file1: '',
+		}  
+    }
 };
 
 function generateTestCases()
@@ -69,9 +78,20 @@ function generateTestCases()
 
 		// update parameter values based on known constraints.
 		var constraints = functionConstraints[funcName].constraints;
+        
 		// Handle global constraints...
-		var fileWithContent = _.some(constraints, {mocking: 'fileWithContent' });
-		var pathExists      = _.some(constraints, {mocking: 'fileExists' });
+		var fileWithContent = _.some(constraints, {mocking: 'fileWithContent' });  // file with content
+		var pathExists      = _.some(constraints, {mocking: 'fileExists' });  // where the path of file exists
+        
+        var emptyFile = _.some(constraints, {mocking: 'emptyFile' });   // for file where the content is empty
+        var varQUndefined = _.some(constraints, {value: 'undefined' });  // variable q = undefined
+        var telNumber = _.some(constraints, {value: '212' });     // to handle the area code of tel number
+        var truthNormalization = _.some(constraints, {value: true });  // for true
+        var liesNormalization = _.some(constraints, {value: false });  // for false
+        var varQZeroValue = _.some(constraints, {value: '0' });  // where q=0
+        
+        
+    
 
 		for( var c = 0; c < constraints.length; c++ )
 		{
@@ -86,16 +106,34 @@ function generateTestCases()
 		var args = Object.keys(params).map( function(k) {return params[k]; }).join(",");
 		if( pathExists || fileWithContent )
 		{
-			content += generateMockFsTestCases(pathExists,fileWithContent,funcName, args);
+			content += generateMockFsTestCases(pathExists,fileWithContent,!emptyFile, funcName, args);
 			// Bonus...generate constraint variations test cases....
-			content += generateMockFsTestCases(!pathExists,!fileWithContent,funcName, args);
-			content += generateMockFsTestCases(pathExists,!fileWithContent,funcName, args);
-			content += generateMockFsTestCases(!pathExists,fileWithContent,funcName, args);
-		}
+			content += generateMockFsTestCases(!pathExists,fileWithContent,!emptyFile,funcName, args);
+			content += generateMockFsTestCases(pathExists,!fileWithContent,!emptyFile,funcName, args);
+			content += generateMockFsTestCases(!pathExists,!fileWithContent,!emptyFile,funcName, args);
+            content += generateMockFsTestCases(pathExists,!fileWithContent,emptyFile,funcName, args);
+        }
+        else if(varQUndefined||varQZeroValue){     // condition when q = 0 OR q=undefined
+            content += "subject.{0}({1});\n".format(funcName, args );
+            content += "subject.{0}({1});\n".format(funcName, "-10,undefined" ); // p= -ve and q=undefined
+            content += "subject.{0}({1});\n".format(funcName, "-11,0" );  // p= -ve and q = 0
+        }  
+        else if(truthNormalization||liesNormalization){   // condition when normalization = true and normalization=false
+            var phoneNum = faker.phone.phoneNumberFormat();
+            var phoneFormat = faker.phone.phoneFormats();
+            var phoneOptions = "{normalize:true}".toString();
+            content += "subject.{0}({1});\n".format(funcName, "'"+phoneNum+"','"+phoneFormat+"',"+ phoneOptions );
+        }
+        else if(!telNumber){
+            var phn = faker.phone.phoneNumberFormat();
+            content += "subject.{0}({1});\n".format(funcName, "'212-366-5986'" ); 
+            content += "subject.{0}({1});\n".format(funcName, "'"+phn+"'" );  
+        }
 		else
 		{
 			// Emit simple test case.
 			content += "subject.{0}({1});\n".format(funcName, args );
+            
 		}
 
 	}
@@ -105,7 +143,7 @@ function generateTestCases()
 
 }
 
-function generateMockFsTestCases (pathExists,fileWithContent,funcName,args) 
+function generateMockFsTestCases (pathExists,fileWithContent,emptyFile,funcName,args) 
 {
 	var testCase = "";
 	// Insert mock data based on constraints.
@@ -118,6 +156,11 @@ function generateMockFsTestCases (pathExists,fileWithContent,funcName,args)
 	{
 		for (var attrname in mockFileLibrary.fileWithContent) { mergedFS[attrname] = mockFileLibrary.fileWithContent[attrname]; }
 	}
+    if(emptyFile)
+    {
+        for (var attrname in mockFileLibrary.emptyFile) { mergedFS[attrname] = mockFileLibrary.emptyFile[attrname]; }
+    }
+
 
 	testCase += 
 	"mock(" +
@@ -163,7 +206,71 @@ function constraints(filePath)
 							});
 					}
 				}
+                
+                
+                if( child.type === 'BinaryExpression' && child.operator == "<")
+				{
+					if( child.left.type == 'MemberExpression' && params.indexOf( child.left.property.name ) == "length")
+					{
+						// get expression from original source code:
+						//var expression = buf.substring(child.range[0], child.range[1]);
+						var rightHand = buf.substring(child.right.range[0], child.right.range[1])
+						functionConstraints[funcName].constraints.push( 
+							{
+								ident: child.left.object.name,
+								value: rightHand
+							});
+					}
+				}
+                
+                if( child.type === 'BinaryExpression' && child.operator == ">")
+				{
+					if( child.left.type == 'MemberExpression' && params.indexOf( child.left.name ) == "length")
+					{
+						// get expression from original source code:
+						//var expression = buf.substring(child.range[0], child.range[1]);
+						var rightHand = buf.substring(child.right.range[0], child.right.range[1])
+						functionConstraints[funcName].constraints.push( 
+							{
+								ident: child.left.object.name,
+								value: rightHand
+							});
+					}
+				}
+               
+                
+              if( child.type == 'LogicalExpression' && child.operator=="||")
+				{
 
+					if(child.left.type=='UnaryExpression')
+					{
+						functionConstraints[funcName].constraints.push(
+							{
+							ident: child.left.argument.name,
+							value: true,
+							}
+							);
+						functionConstraints[funcName].constraints.push(
+							{
+							ident: child.left.argument.name,
+							value: false,
+							}
+							);
+					}
+
+				if(child.right.type=='UnaryExpression' && child.right.operator == "!"){
+					if(child.right.argument.type=='MemberExpression'){
+					functionConstraints[funcName].constraints.push(
+						{
+						ident: child.right.argument.object.name+'.'+child.right.argument.property.name,
+						value: true,
+						}
+						);
+					
+					}
+				}
+			}
+                
 				if( child.type == "CallExpression" && 
 					 child.callee.property &&
 					 child.callee.property.name =="readFileSync" )
@@ -178,6 +285,26 @@ function constraints(filePath)
 								ident: params[p],
 								value: "'pathContent/file1'",
 								mocking: 'fileWithContent'
+							});
+						}
+					}
+				}
+                
+                
+                	if( child.type == "CallExpression" && 
+					 child.callee.property &&
+					 child.callee.property.name =="readFileSync" )
+				{
+					for( var p =0; p < params.length; p++ )
+					{
+						if( child.arguments[0].name == params[p] )
+						{
+							functionConstraints[funcName].constraints.push( 
+							{
+								// A fake path to a file
+                                ident: params[p],
+								value: "'pathContent/file1'",
+								mocking: 'emptyFile'
 							});
 						}
 					}
@@ -201,6 +328,7 @@ function constraints(filePath)
 						}
 					}
 				}
+                
 
 			});
 
--- subject.js
+++ subject.js
@@ -1,11 +1,11 @@
 var fs = require("fs");
 
 function inc(p, q){
-    if(q ==undefined) q =1;
+    if(q ==undefined) q =1;  // c1     // pass the second value as undefined
 
-   if( p < 0 )
+   if( p < 0 )  // c2                 // pass the first value less than 0
    {
-   	p = -p;
+   	p = -p;  //c3                     
    }
 
     return p + q/q;
@@ -14,18 +14,18 @@ function inc(p, q){
 
 function fileTest(dir, filePath)
 {
-	if (!fs.existsSync(dir)){
+	if (!fs.existsSync(dir)){  // existsSync checks whether a file exists in the node.js directory
    	return false;
 	}
 
    if( fs.existsSync(filePath ))
    {
-		var buf = fs.readFileSync(filePath, "utf8");
-		if( buf.length > 0 )
+		var buf = fs.readFileSync(filePath, "utf8");  // readFileSync reads from the file
+		if( buf.length > 0 )  //c4                    // give a valid filePath while calling this function such that the file has some contents
 		{
 			return true;
 		}
-		return false;
+		return false;   //c5                     // give a valid file path such that the file is empty (i.e. has no contents)
 	}
 }
 
@@ -36,17 +36,17 @@ function normalize(phoneNumber) {
       "$1$2$3"
     );
 
-}
+}   
 
 function format(phoneNumber, formatString, options) 
 {
     // Normalize the phone number first unless not asked to do so in the options
-    if (!options || !options.normalize) {
+    if (!options || !options.normalize) {   //c6
       phoneNumber = normalize(phoneNumber)
     };
 
-    for ( var i = 0, l = phoneNumber.length; i < l; i++ ) {
-      formatString = formatString.replace("N", phoneNumber[i]);
+    for ( var i = 0, l = phoneNumber.length; i < l; i++ ) {  // c7
+      formatString = formatString.replace("N", phoneNumber[i]);  // c8
     }
   
     return formatString;
@@ -56,10 +56,11 @@ function format(phoneNumber, formatString, options)
 function blackListNumber(phoneNumber)
 {
 	var num = format(phoneNumber, "(NNN) NNN-NNNN");
-	var area = num.substring(1,4);
-	if( area == "212" )
+	var area = num.substring(1,4);                        // get the area code
+	if( area == "212" )   //c9                            // enter a number such that the area code is "212"
 	{
-		return true;
+		return true;  // c10
+
 	}
 	return false;
 }
@@ -69,4 +70,12 @@ exports.normalize = normalize;
 exports.format = format;
 exports.inc = inc;
 exports.blackListNumber = blackListNumber;
+/*
+Exports is used to make parts of your module available to scripts outside the module. So when someone uses require like below in another script:
+var sys = require("sys");  
+They can access any functions or properties you put in module.exports
 
+Here we have var fs = require("fs");
+hence the fs properties such as fileTest, normalize, format, inc and blackListNumber are available to fs module
+
+*/
